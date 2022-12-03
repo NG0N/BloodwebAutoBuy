0cec0a975b5f9cc628d875c813dbab6bf3487c7c
NG0N <20257056+NG0N@users.noreply.github.com>
2022-12-03 12:46:24 +0200

    Clean up config menu

--- src/autobuy/__main__.py
+++ src/autobuy/__main__.py
@@ -1,4 +1,4 @@
-from gooey import Gooey, GooeyParser
+from gooey import Gooey, GooeyParser, local_resource_path
 
 from web_autobuy import Autobuy
 
@@ -8,15 +8,17 @@ from web_analyzer import WebAnalyzer
 
 @Gooey(
     program_name='Bloodweb AutoBuy',
-    program_description="Automated Bloodweb progression for Dead by Daylight",
-    #image_dir='data',
-    body_bg_color="#BBBDC0",
-    header_bg_color="#C9B899",
-    footer_bg_color="#717E92",
+    program_description='Automated Bloodweb progression for Dead by Daylight',
+    image_dir=local_resource_path('data/images'),
+    body_bg_color='#FFFFFF',
+    header_bg_color='#C9B899',
+    show_success_modal = False,
+    footer_bg_color='#717E92',
     tabbed_groups=True,
     show_stop_warning=False,
     force_stop_is_error=False,
     show_sidebar=False,
+    clear_before_run = True,
     default_size=(600, 700),
     menu=[{
         'name': 'Help',
@@ -30,112 +32,136 @@ from web_analyzer import WebAnalyzer
 def main():
     parser = GooeyParser()
     options_group = parser.add_argument_group(
-    "Options",
-"""Moving your mouse or pressing F3 will pause the program, press F3 again to resume.
-Press Esc or F2 to stop the program at any point.
-Make sure that the game is in fullscreen mode and that the GUI scale is set to 100%.""")
+                        'Options', description = gui_menu.options_group_desc)
     advanced_group = parser.add_argument_group(
-    "Advanced", 
-    """Customize the color detection parameters, might be required if you use strong filters. Disabling filters temporarily instead is probably easier.
-If your monitor resolution is unsupported, you can calibrate the sample points yourself
-
-All you have to do is to supply the X and Y pixel coordinates of the bloodweb center
-Use the 'Save test images' setting with the Bloodweb open to see the results""")
+                        'Advanced', 
+                        description = gui_menu.advanced_group_desc)
+                        
+    ordering = options_group.add_mutually_exclusive_group(
+                    gooey_options = {
+                        'initial_selection': 0
+                        }
+                    )
     
+    ordering.add_argument('-c', '--cheap',
+                            metavar='Cheap Mode',
+                            action='store_true', 
+                            help='Buy the most common nodes first')
+    ordering.add_argument('-e', '--expensive',
+                            metavar='Expensive Mode',
+                            action='store_true', 
+                            help='Buy the rarest nodes first.')
+                                
+    ordering.add_argument('-s', '--shuffle',
+                            metavar='Random Mode',
+                            action='store_true', 
+                            help='Buy the nodes in a random order.')
     
     options_group.add_argument('-m', '--monitor_index',
                             default=0,
                             metavar='Monitor index',
-                            help="Leave to 0 to let the game window be found automatically.",
+                            help='Leave to 0 to let the game window be found automatically.',
                             widget='IntegerField')
     
     options_group.add_argument('-w', '--activate_window',
                         default=True,
-                        action="store_true", 
-                        metavar="Bring window to foreground",
-                        help="Focuses the game window at the start to make sure no other windows are covering it")
+                        action='store_true', 
+                        metavar='Bring window to foreground',
+                        help='Focuses the game window at the start to make sure no other windows are covering it')
 
     options_group.add_argument('-p', '--start_paused',
                             default=False,
                             metavar='Start Paused',
-                            action="store_true", 
-                            help="""Start the program in the paused state. Pressing F3 is required to start.""")
+                            action='store_true', 
+                            help='Start the program in the paused state. Pressing F3 is required to start.')
         
 
-    ordering = options_group.add_mutually_exclusive_group()
-    
-    ordering.add_argument('-e', '--expensive',
-                            metavar='Buy the most expensive nodes first',
-                            action="store_true", 
-                            help="""Buy the rarest nodes first. Leave off to buy cheaper nodes first""")
-                        
-    ordering.add_argument('-s', '--shuffle',
-                            metavar="Randomize order",
-                            action="store_true", 
-                            help="Buy the nodes in a random order.")
+
     
     options_group.add_argument('--should_prestige',
                             default=True,
-                            action="store_true", 
+                            action='store_true', 
                             metavar='Auto-Prestige',
-                            help="Automatically advance prestige levels. Disable to pause after level 50")
+                            help='Automatically advance prestige levels. Disable to pause after level 50')
     
     
     options_group.add_argument('-t', '--time_limit',
                             metavar='Time limit',
                             default=0.0,
-                            widget="DecimalField",
-                            help="Stop after this duration (minutes), Set to 0 to disable limit.")
+                            widget='DecimalField',
+                            help='Stop after this duration (minutes), Set to 0 to disable limit.')
     
     advanced_group.add_argument('--ring_color',
-                                default="#918b6a",
+                                default='#918b6a',
                                 metavar='Purchasable node ring color',
-                                help="Customize the color that's used to detect whether a node is purchasable.\nSampled in the middle of a node's yellow ring. Default: [R: 145, G: 139, B: 106]", 
+                                help="""Customize the color that's used to detect whether a node is purchasable.
+Sampled in the middle of a node's yellow ring. Default: [R: 145, G: 139, B: 106]""", 
                                 widget='ColourChooser') 
 
     advanced_group.add_argument('--node_color_threshold',
                             default=20,
                             metavar='Node color detection threshold',
-                            help="Customize the detection tolerance for nodes. Too high values can result in false positives.\nDefault: 20",
-                            widget='IntegerField')    
+                            help="""Customize the detection tolerance for nodes. Too high values can result in false positives.
+Default: 20""",
+                            widget='Slider',
+                            gooey_options = {
+                                    'min' : 0, 
+                                    'max' : 100, 
+                                    'increment' : 1
+                            })
     
     advanced_group.add_argument('-v', '--verbose',
                                 metavar='Verbose output',
-                                action="store_true", 
-                                help="""Print debug info.""")
+                                action='store_true', 
+                                help='Print debug info.')
     
     advanced_group.add_argument('--unsupported_resolution_debug',
                                 metavar='For unsupported resolutions: Save test images',
                                 default=False,
-                                action="store_true", 
-                                help="""Turn on to save test images to preview the custom midpoint. Files beginning with BAB_<x>_<y> will be saved on the desktop.""",
-                                widget="BlockCheckbox")
+                                action='store_true', 
+                                help='Turn on to save test images to preview the custom midpoint. Files beginning with BAB_<x>_<y>.png will be saved on the desktop.',
+                                widget='BlockCheckbox',
+                                gooey_options = {
+                                    'checkbox_label' : "Enable test mode"
+                                 })
     
     
     advanced_group.add_argument('--unsupported_resolution_mid_x',
                                 metavar='For unsupported resolutions: Midpoint X',
                                 default=0,
-                                help="""X coordinate of the Bloodweb midpoint"""
-                                )
+                                help='X coordinate of the Bloodweb midpoint',
+                                widget='IntegerField',
+                                gooey_options = {
+                                    'min' : 0, 
+                                    'max' : 12800, 
+                                    'increment' : 1
+                                })
+
                                 
 
     advanced_group.add_argument('--unsupported_resolution_mid_y',
                                 metavar='For unsupported resolutions: Midpoint Y',
                                 default=0,
-                                help="""Y coordinate of the Bloodweb midpoint""")
+                                help='Y coordinate of the Bloodweb midpoint',
+                                widget='IntegerField',
+                                gooey_options = {
+                                    'min' : 0, 
+                                    'max' : 12800, 
+                                    'increment' : 1
+                                })
 
 
     args = parser.parse_args()
 
     if args.unsupported_resolution_debug:
-        
+        # Run the custom resolution debug image generator 
         analyzer = WebAnalyzer()
         analyzer.set_bring_to_front(bool(args.activate_window))
         analyzer.set_override_monitor_index(int(args.monitor_index))
         analyzer.set_custom_midpoint(int(args.unsupported_resolution_mid_x), int(args.unsupported_resolution_mid_y))
         try:
             analyzer.initialize()
-        except:
+        except WebAnalyzer.GameResolutionError:
             print("Failed to initialize", flush=True)
             return
         analyzer.save_debug_images()
--- src/autobuy/gui_menu.py
+++ src/autobuy/gui_menu.py
@@ -1,3 +1,12 @@
+options_group_desc = """Moving your mouse or pressing F3 will pause the program, press F3 again to resume.
+Press Esc or F2 to stop the program at any point.
+Make sure that the game is in fullscreen mode and that the GUI scale is set to 100%."""
+
+advanced_group_desc = """Customize the color detection parameters, might be required if you use strong filters. Disabling filters temporarily instead is probably easier.
+If your monitor resolution is unsupported, you can calibrate the sample points yourself:
+Supply the X and Y pixel coordinates of the Bloodweb center pooint, relative to the game window's top left corner
+Use the 'Save test images' setting with the Bloodweb open to test the alignment"""
+
 
 help_items = [
 {
