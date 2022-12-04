cfa409c81efe5f7f221f989780fe0df0eb87ca08
NGON <20257056+NG0N@users.noreply.github.com>
2022-12-04 17:40:25 +0200

    Improve node edge detection, make testing better

--- src/autobuy/web_analyzer.py
+++ src/autobuy/web_analyzer.py
@@ -7,6 +7,9 @@ from PIL import Image, ImageDraw
 from pathlib import Path
 import sys
 from os import getcwd
+from argparse import ArgumentParser
+
+
 class GameWindow:
     handle : int
     position : np.ndarray[int]
@@ -20,16 +23,20 @@ class GameWindow:
 # Reference resolution is used in sample point coordinates
 # The points are scaled from this resolution to whatever the game window size is
 REF_RESOLUTION = (2560,1440)
+
 ### These values are only valid for 2560x1440 and are scaled during runtime
-# Diameter of node rings
-NODE_SIZE = 100
 # Width of the square of pixels that is used to determine the node rarity
 # Scaled by resolution
 RARITY_CROP_SIZE = 30
+
 # Offset to the node edge where the color is sampled to determine if the node can be bought
-NODE_EDGE_OFFSET = np.array([-46,20], int)
+NODE_EDGE_OFFSET = np.array([-46,20], float)
 # Not implemented, offset to the white perk icon stripe
 #PERK_SAMPLE_OFFSET = np.array([24,-38], int)
+
+# Diameter of node rings
+NODE_SIZE = 106
+
 
 # Used to parse the sample point file
 NODE_COUNT = 30
@@ -70,7 +77,7 @@ RARITIES_HUE = np.array([
 
 
 
-
+PRESTIGE_ONLY = np.array([-1],int)
 
 class WebAnalyzer:
     # Default node edge color
@@ -108,6 +115,8 @@ class WebAnalyzer:
     
     # Center position for current resolution, in game window space
     _center_pos : np.ndarray[int]
+    
+    _test_image : Image.Image = None
     
     class GameResolutionError(Exception):
         resolution: str = ""
@@ -157,7 +166,12 @@ class WebAnalyzer:
     
     def set_bring_to_front(self, bring_to_front : bool):
         self._bring_to_front = bring_to_front
-        
+    
+    def set_test_image(self, image: Image.Image):
+        self._test_image = image
+
+
+              
     # Captures a screenshot
     # bbox is start and end positions. Game window position offset is added here
     # Returns in BGR format 
@@ -177,21 +191,43 @@ class WebAnalyzer:
             return self._center_pos + self._game_window.position
         return self._web_nodes[node] + self._game_window.position
     
+    # Checks a horizontal line of around each of the positions
+    # Returns the indices that had pixels close to the given color
+    def _get_positions_approx_color(self, image, positions, color, tolerance, sampling_radius):
+        start1 = positions[:,1] - sampling_radius
+        end1 = positions[:,1] + sampling_radius
+        
+        # Extract lines around each of the positions
+        rim_imgs = np.zeros((len(start1),2 * sampling_radius, 3),int)
+        for i in range(len(start1)):
+            s1 = start1[i]
+            e1 = end1[i]
+            rim_imgs[i] = image[s1:e1,positions[i,0],:]
+            
+        diffs = rim_imgs - color
+        dists = np.linalg.norm(diffs, axis=(2))
+        min_dists = np.min(np.abs(dists),axis=1)
+        return (min_dists < tolerance).nonzero()[0]
+    
     # Takes a screen capture, samples the node positions, sorts by rarity, most common first
     # 0-29 are normal nodes, -1 is prestige node
     # Returns None if no nodes detected
-    def find_buyable_nodes(self):
+    def find_buyable_nodes(self) -> np.ndarray:
         bbox = self._web_bbox
            
         # Convert to python int tuple for MSS
         capture_bbox = (bbox[0][0].item(), bbox[0][1].item(), bbox[1][0].item(), bbox[1][1].item())
-        image = self.capture(capture_bbox)
-        rim_positions = self._web_points - bbox[0]
-        samples = image[rim_positions[:,1],rim_positions[:,0]]
-        dists = np.linalg.norm(np.subtract(samples, self._color_node_available), axis=1)
-        buyable = np.asarray(dists < self._color_tolerance).nonzero()[0]
         
+        if not self._test_image:
+            image = self.capture(capture_bbox)
+        else:
+            cropped = self._test_image.crop(capture_bbox)
+            image = np.asarray(cropped)[:,:,::-1] # RGB to BGR
         
+        edge_positions = self._web_points - bbox[0]
+        # Get indices of the positions that are close to the color of a buyable node
+        buyable = self._get_positions_approx_color(image, edge_positions, self._color_node_available, self._color_tolerance, 2)
+
         # Extract node images
         node_positions = self._web_nodes[buyable] - bbox[0]
         
@@ -243,7 +279,7 @@ class WebAnalyzer:
         diffs = np.subtract(samples, COLOR_PRESTIGE_SMALL)
         dists = np.linalg.norm(diffs, axis=1)
         if np.max(dists, axis=0) < self._color_tolerance * 1.2:
-            return [-1]
+            return PRESTIGE_ONLY
 
         # Check for large prestige node
         sample_positions = self._large_prestige_points - bbox[0]
@@ -251,7 +287,7 @@ class WebAnalyzer:
         diffs = np.subtract(samples, COLOR_PRESTIGE_LARGE)
         dists = np.linalg.norm(diffs, axis=0)
         if np.max(dists, axis=0) < self._color_tolerance + max(self._color_tolerance * 1.5, 20):
-            return [-1]
+            return PRESTIGE_ONLY
         
     # Find minimum angle difference in hue    
     def _find_closest_rarity(self, hue):
@@ -268,16 +304,16 @@ class WebAnalyzer:
                 res_pair = pair[0].split("x", 1)
                 pos_pair = pair[1].split(",", 1)
                 resolution = (int(res_pair[0]), int(res_pair[1]))
-                center_pos = np.array([int(pos_pair[0]), int(pos_pair[1])], int)
+                center_pos = np.array([float(pos_pair[0]), float(pos_pair[1])], float)
                 center_points[resolution] = center_pos
         return center_points
 
     # Draws and saves an image file for debugging the currently loaded sample points
     def debug_draw_points(self, groups_to_show: list) -> Image.Image:
         # Add padding for drawing
-        bbox = (self._web_bbox[0] - int(NODE_SIZE * 0.5),
-                self._web_bbox[1] + int(NODE_SIZE * 0.5))
-        
+        bbox = (self._web_bbox[0] - int(NODE_SIZE * 0.5 * self._scaling),
+                self._web_bbox[1] + int(NODE_SIZE * 0.5 * self._scaling))
+        draw_origin = bbox[0]
         bbox_size = bbox[1] - bbox[0]
 
         # Size of the area that is cropped from the original capture
@@ -288,8 +324,8 @@ class WebAnalyzer:
         zoom_paste_size: int = zoom_crop_size * 3
         zoom_paste_radius: int = int(zoom_crop_size * 3 * 0.5)
         
-        node_size = int(106 * self._scaling)
-        node_radius = int(106 * self._scaling * 0.5)
+        node_size = int(NODE_SIZE * self._scaling)
+        node_radius = int(NODE_SIZE * self._scaling * 0.5)
         # Create a circle mask
         mask = Image.new('1', (zoom_paste_size,zoom_paste_size),0)
         mask_draw = ImageDraw.Draw(mask)
@@ -305,12 +341,16 @@ class WebAnalyzer:
                         bbox[0][1].item(),
                         bbox[1][0].item(),
                         bbox[1][1].item())
-        image = self.capture(capture_bbox)
         
-        # BGR to RGB
-        rgb = image[:,:,::-1]
-        
-        im = Image.fromarray(rgb)
+        if not self._test_image:
+            image = self.capture(capture_bbox)
+            # BGR to RGB
+            image = image[:,:,::-1]
+        else:
+            cropped = self._test_image.crop(capture_bbox)
+            image = np.asarray(cropped)
+
+        im = Image.fromarray(image)
         
         pts_groups = {
             "edges" : self._web_points,
@@ -327,17 +367,27 @@ class WebAnalyzer:
             "prestige_large" : (0,255,255),
         }
         
+        if groups_to_show.count("edges") > 0:
+            buyable = self.find_buyable_nodes()
+            print(buyable)
         draw = ImageDraw.Draw(im, "RGBA")
         for pt_group in groups_to_show:
             pts = pts_groups[pt_group]
             for i in range(pts.shape[0]):
-                pos = pts[i] - (self._web_bbox[0] - int(NODE_SIZE * 0.5)) #+ [39,1]
+                pos = pts[i] - draw_origin #+ [39,1]
                 if pt_group == "nodes":
                     node_draw_pos = (pos[0] - node_radius, pos[1] - node_radius)
                     draw.ellipse((node_draw_pos, (node_draw_pos[0] + node_size, node_draw_pos[1] + node_size)), fill=group_colors[pt_group])
                 else:
                     # Draw sample point
-                    im.putpixel(pos, sample_pos_color)
+                    pixel_col = sample_pos_color
+                    outline_col = group_colors[pt_group]
+                    
+                    if pt_group == "edges" and i in buyable:
+                        pixel_col = (0,255,0)
+                        outline_col = (0,255,0)
+                        
+                    im.putpixel(pos, pixel_col)
                     
                     # Crop out the area around the sample point
                     crop_bbox = (pos[0] - zoom_crop_radius, pos[1] - zoom_crop_radius, pos[0] + zoom_crop_radius, pos[1] + zoom_crop_radius)
@@ -346,7 +396,7 @@ class WebAnalyzer:
                     # Paste onto the original capture and draw an outline
                     paste_pos = (pos[0] - zoom_paste_radius, pos[1] - zoom_paste_radius)
                     im.paste(zoomed, paste_pos, mask)
-                    draw.ellipse((paste_pos, (paste_pos[0] + zoom_paste_size, paste_pos[1] + zoom_paste_size)), outline=group_colors[pt_group], width=2)
+                    draw.ellipse((paste_pos, (paste_pos[0] + zoom_paste_size, paste_pos[1] + zoom_paste_size)), outline=outline_col, width=2)
 
         return im
 
@@ -378,32 +428,33 @@ class WebAnalyzer:
                 except:
                     err.resolution = ""
                 raise err
-            # This is stored for prestiging
+            # Center pos is stored for prestiging
             if has_custom_midpoint:
                 self._center_pos = self._custom_midpoint
                 print(f"Using custom midpoint {self._custom_midpoint}", flush=True)
             else:
                 self._center_pos = center_points[resolution] 
             # Remove web position from the points so they are centered around [0,0]
-            local_pts = (self._sample_points - ref_center).astype(np.float64)
+            local_pts = (self._sample_points - ref_center).astype(float)
             # Scale according to game window width
             self._scaling = resolution[0] / REF_RESOLUTION[0]
             local_pts *= self._scaling
             # Add the web offset back
-            self._sample_points = (local_pts + self._center_pos).astype(int)
-
+            self._sample_points = np.round(local_pts + self._center_pos).astype(int)
+            
         # Precalculate scaling dependent values  
         self._rarity_sample_width = int(RARITY_CROP_SIZE * self._scaling)
         # Create array views for iterating
         self._web_nodes = self._sample_points[:NODE_COUNT]
-        self._web_points = self._sample_points[:NODE_COUNT] + (NODE_EDGE_OFFSET * self._scaling).astype(int)
+        self._web_points = self._sample_points[:NODE_COUNT] + np.round(NODE_EDGE_OFFSET * self._scaling).astype(int)
         self._small_prestige_points = self._sample_points[NODE_COUNT:
             NODE_COUNT + SAMPLE_COUNT_SMALL_PRESTIGE]
         self._large_prestige_points = self._sample_points[NODE_COUNT + SAMPLE_COUNT_SMALL_PRESTIGE:
             NODE_COUNT + SAMPLE_COUNT_SMALL_PRESTIGE + SAMPLE_COUNT_LARGE_PRESTIGE]
 
     def _calculate_bounds(self):
-        padding = int(NODE_SIZE * 0.5 * self._scaling)
+        dist_to_sample_pos = np.ceil(np.linalg.norm(NODE_EDGE_OFFSET))
+        padding = int(dist_to_sample_pos * self._scaling)
         min, max = np.min(self._sample_points, axis=0), np.max(self._sample_points, axis=0)
         min -= padding
         max += padding
@@ -414,6 +465,12 @@ class WebAnalyzer:
         return self._game_window.position + self._web_bbox[0]
     
     def _update_game_window_info(self):
+        if self._test_image:
+            self._game_window = GameWindow(None,
+                    np.array([0, 0], int),
+                    np.array([self._test_image.width, self._test_image.height], int))
+            return
+        
         self._game_window = None
         # If set, override the window with the given monitor index
         if self._override_monitor_index > 0:
@@ -476,11 +533,57 @@ class WebAnalyzer:
         except:
             print("Could not save images: Failed to save file")
             
+
+
+# Testing functionality
+def main_test():
+    parser = ArgumentParser("Bloodweb Analyzer", description="Test the analyzer")
+    parser.add_argument("-t", "--test_images", nargs='+', required=False)
+    parser.add_argument("-d", "--draw_tests", action='store_true')
+    args = parser.parse_args()
+    #args.test_images = ["./2560x1440_test.png", "./3840x2400_test.png", "./1360x768_test.png"]
+    #args.draw_tests = False
+    if args.test_images:
+        # Use image(s)
+       run_batch_image_test(args.test_images, args.draw_tests)
+    else:
+        # Capture from game screen
+        run_test(args.draw_tests)
+
+
+def run_batch_image_test(images: list, draw: bool):
+    # Test using images
+    paths = []
+    for p in images:
+        path = Path(p)
+        if not path.is_file():
+            print(f"Invalid test_image: {path.absolute()}")
+            continue
+        paths.append(path) 
+        
+    for path in paths:
+        try:
+            image = Image.open(path)
+        except Exception as err:
+            print(f"Failed to open test_image: {path.absolute()}")
+            print(err)
+            continue
+        run_test(draw, image)
     
-    
-if __name__ == "__main__":
+
+def run_test(draw: bool, image = None):
     analyzer = WebAnalyzer()
+    if image:
+        analyzer.set_test_image(image)
     analyzer.initialize()
-    im = analyzer.debug_draw_points(["edges"])
-    im.save(f"out_{analyzer._game_window.size}.png")
-    print(f"Valid nodes: {analyzer.find_buyable_nodes()}")
+    if draw:
+        im = analyzer.debug_draw_points(["nodes", "edges"])
+        outpath = f"test_out_{analyzer._game_window.size[0]}x{analyzer._game_window.size[1]}.png"
+        im.save(outpath)
+        print(f"Image saved to {outpath}")
+    else:
+        print(f"Valid nodes: {analyzer.find_buyable_nodes()}")
+
+
+if __name__ == "__main__":
+    main_test()
